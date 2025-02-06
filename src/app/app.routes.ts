7e9e4d1c19bd99d73c485b1c966499422a07a6c2
HelmutM87 <127511280+HelmutM87@users.noreply.github.com>
2025-02-06 16:37:05 -0300

    changed the patients database

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PatientsComponent } from './patients/patients.component';
 import { SettingsComponent } from './settings/settings.component';
-import { PatientDetailsComponent } from './patient-details/patient-details.component'; // Import der Detailansicht-Komponente
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'patients', component: PatientsComponent },
-  { path: 'patients/:id', component: PatientDetailsComponent }, // Neue Route für die Detailansicht mit ID-Parameter
   { path: 'settings', component: SettingsComponent },
 ];
--- src/app/patients/patients.component.ts
+++ src/app/patients/patients.component.ts
@@ -2,19 +2,24 @@ import { Component, inject } from '@angular/core';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table'; // Import für MatTableModule
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
 
 @Component({
   selector: 'app-patients',
   standalone: true,
   imports: [
     CommonModule,
-    MatListModule,
-    MatCardModule,
-    MatDividerModule
+    MatTableModule, // MatTableModule hinzufügen
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   templateUrl: './patients.component.html',
   styleUrls: ['./patients.component.css']
@@ -22,6 +27,7 @@ import { CommonModule } from '@angular/common';
 export class PatientsComponent {
   private firestore: Firestore = inject(Firestore);
   patients$: Observable<any[]>;
+  displayedColumns: string[] = ['idNumber', 'lastName', 'firstName', 'birthDate', 'insuredSince', 'paymentMethod']; // Definieren der anzuzeigenden Spalten
 
   constructor() {
     const patientsCollection = collection(this.firestore, 'customers');
