e708e31a52a4a527d1c165652b0ff09a824a1baf
HelmutM87 <127511280+HelmutM87@users.noreply.github.com>
2025-02-06 16:17:29 -0300

    changed the dialog-add-customer

--- src/app/dialog-add-customer/dialog-add-customer.component.ts
+++ src/app/dialog-add-customer/dialog-add-customer.component.ts
@@ -1,12 +1,29 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { Firestore, collection, addDoc, getFirestore } from '@angular/fire/firestore';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { v4 as uuidv4 } from 'uuid';
+import { MatDialogTitle, MatDialogContent, MatDialogActions } from '@angular/material/dialog';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { NativeDateAdapter, DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { Platform } from '@angular/cdk/platform';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import * as moment from 'moment'; // Korrekter Import
+
+const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD.MM.YYYY',
+  },
+  display: {
+    dateInput: 'DD.MM.YYYY',
+  },
+};
+
 
 @Component({
   selector: 'app-dialog-add-customer',
@@ -15,9 +32,16 @@ import { MatButtonModule } from '@angular/material/button';
     ReactiveFormsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCheckboxModule,
     MatSelectModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogTitle, MatDialogContent, MatDialogActions,
+    MatCheckboxModule,
+    MatDatepickerModule
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'de-DE' },
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE, Platform] },
+    { provide: MAT_DATE_FORMATS, useValue: MY_DATE_FORMATS },
   ],
   templateUrl: './dialog-add-customer.component.html',
   styleUrls: ['./dialog-add-customer.component.css']
@@ -27,28 +51,34 @@ export class DialogAddCustomerComponent {
 
   constructor(
     private fb: FormBuilder,
-    private firestore: Firestore, // Firestore direkt injizieren
-    public dialogRef: MatDialogRef<DialogAddCustomerComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    private firestore: Firestore,
+    public dialogRef: MatDialogRef<DialogAddCustomerComponent>
   ) {
     this.customerForm = this.fb.group({
-      idNumber: ['', Validators.required],
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       birthDate: ['', Validators.required],
+      city: ['', Validators.required],
+      documentNumber: ['', Validators.required],
+      phoneNumber: ['', Validators.required],
       insuredSince: ['', Validators.required],
-      insuranceStatus: [false, Validators.required],
-      paymentMethod: ['', Validators.required]
+      paymentMethod: ['', Validators.required],
+      insuranceStatus: [false]
     });
   }
 
   async saveCustomer() {
     if (this.customerForm.valid) {
-      const db = getFirestore(); // Sicherstellen, dass Firestore verwendet wird
-      const customersCollection = collection(db, 'customers'); 
-      
-      await addDoc(customersCollection, this.customerForm.value);
-      this.dialogRef.close();
+      const db = getFirestore();
+      const customersCollection = collection(db, 'customers');
+
+      const customerData = {
+        ...this.customerForm.value,
+        insuredNumber: uuidv4()
+      };
+
+      await addDoc(customersCollection, customerData);
+      this.dialogRef.close(customerData);
     }
   }
 
